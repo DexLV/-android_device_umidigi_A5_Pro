c40a79a994cc5f060b9e48b6dcc6cf6306dd16f2
DexLV <dnb@reborn.com>
2021-04-13 21:38:41 +0300

    Add MTK privapp permissions file

--- device.mk
+++ device.mk
@@ -128,6 +128,16 @@ PRODUCT_PACKAGES += \
     WifiOverlay \
     TetheringConfigOverlay
 
+# Telephony Jars
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common    
+
 # Shims
 PRODUCT_PACKAGES += \
     libshim_showlogo
